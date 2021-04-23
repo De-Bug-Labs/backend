85a9229fd88fdce5b07b394ed201ea658f2909e5
jimmedto <A01700190@itesm.mx>
2021-04-23 13:29:31 -0500

    user creation

---
+++ src/api/UserController.ts
@@ -0,0 +1,26 @@
+import { getManager } from 'typeorm';
+import { User } from '../orm/entity/user';
+import { Role } from '../orm/entity/role';
+
+const userRepo = getManager().getRepository(User);
+const roleRepo = getManager().getRepository(Role);
+
+export const createUser = async (req, res): Promise<void> => {
+    try {
+		let usr = new User();
+		usr = req.swagger.params.user.raw;
+		usr.name = req.swagger.params.user.raw.name;
+		usr.lastName = req.swagger.params.user.raw.lastName;
+        usr.email = req.swagger.params.user.raw.email;
+        usr.password = req.swagger.params.user.raw.password;
+        const roleId = req.swagger.params.staff.raw.roleId;
+        const role = await roleRepo.findByIds(roleId);
+        usr.roles = role;
+		console.info(usr);
+		const insert = await userRepo.save(usr);
+		res.status(201).json(insert);
+	} catch (e) {
+		console.info(e);
+		res.status(400).json(e);
+	}
+}
