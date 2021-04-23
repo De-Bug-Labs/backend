7209f0e755f7ed21ea2415579adab54eecb4e131
AlbertoMatuteB <a01704584@itesm.mx>
2021-04-23 14:48:11 -0500

    creacion entidad informacion para el modulo de informacion

---
+++ src/orm/entity/information.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Information {
+	@PrimaryGeneratedColumn('uuid')
+	public id!: string;
+
+	@Column({ type: 'text', nullable: false })
+	public mision!: string;
+
+	@Column({ type: 'text', nullable: false })
+	public instalation!: string;
+
+    @Column({ type: 'text', nullable: false })
+	public team!: string;
+}
