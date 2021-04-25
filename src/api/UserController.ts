7eb63bb49989fbf17a03e6b4d6b35dddccb10c9e
jimmedto <A01700190@itesm.mx>
2021-04-24 20:40:33 -0500

    Added create new user endpoint

--- src/api/UserController.ts
+++ src/api/UserController.ts
@@ -6,16 +6,16 @@ const userRepo = getManager().getRepository(User);
 const roleRepo = getManager().getRepository(Role);
 
 export const createUser = async (req, res): Promise<void> => {
-    try {
+	try {
 		let usr = new User();
 		usr = req.swagger.params.user.raw;
-		usr.name = req.swagger.params.user.raw.name;
-		usr.lastName = req.swagger.params.user.raw.lastName;
-        usr.email = req.swagger.params.user.raw.email;
-        usr.password = req.swagger.params.user.raw.password;
-        const roleId = req.swagger.params.staff.raw.roleId;
-        const role = await roleRepo.findByIds(roleId);
-        usr.roles = role;
+		// usr.name = req.swagger.params.user.raw.name;
+		// usr.lastName = req.swagger.params.user.raw.lastName;
+		// usr.email = req.swagger.params.user.raw.email;
+		usr.password = req.swagger.params.user.raw.password;
+		const roleId = req.swagger.params.user.raw.roleId;
+		const role = await roleRepo.findByIds(roleId);
+		usr.roles = role;
 		console.info(usr);
 		const insert = await userRepo.save(usr);
 		res.status(201).json(insert);
@@ -23,4 +23,8 @@ export const createUser = async (req, res): Promise<void> => {
 		console.info(e);
 		res.status(400).json(e);
 	}
-}
+};
+
+export const deleteUser = async (req, res): Promise<void> => {
+	res.status(501);
+};
