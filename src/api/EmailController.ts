f3cef25b892920365746493a534251dc2c8e62e9
AlbertoMatuteB <a01704584@itesm.mx>
2021-05-02 20:49:12 -0500

    ya se pueden enviar correos, siguiente paso, llenar informacion con correo de instituto

--- src/api/EmailController.ts
+++ src/api/EmailController.ts
@@ -1,6 +1,9 @@
 import { getManager } from 'typeorm';
 import { Department } from '../orm/entities';
 import { PostRegister } from '../orm/entity/postRegister';
+
+var nodemailer = require('nodemailer');
+
 const departmentRepo = getManager().getRepository(Department);
 const registerRepo = getManager().getRepository(PostRegister);
 
@@ -12,6 +15,7 @@ export const createRegisterEmail = async (req, res): Promise<void> => {
 		let email = new PostRegister();
 		email = req.swagger.params.postRegister.raw;
 		const department = req.swagger.params.postRegister.raw.departmentId;
+		const message = req.swagger.params.postRegister.raw.description;
 		const staff = (
 			await departmentRepo.findOneOrFail({
 				relations: ['staff'],
@@ -24,6 +28,29 @@ export const createRegisterEmail = async (req, res): Promise<void> => {
 		email.date = new Date();
 		const insert = await registerRepo.save(email);
 		res.status(201).json(insert);
+
+		var transporter = nodemailer.createTransport({
+			service: 'gmail',
+			auth: {
+			  user: 'user@mail.com',
+			  pass: 'pass@pass'
+			}
+		  });
+		  
+		  var mailOptions = {
+			from: 'user@mail.com',
+			to: 'userreceiver@mail.com', //buscar una forma de poner el staff aqui
+			subject: 'Sending Email using Node.js',
+			text: message //esto ya esta resuelto
+		  };
+		  
+		  transporter.sendMail(mailOptions, function(error, info){
+			if (error) {
+			  console.log(error);
+			} else {
+			  console.log('Email sent: ' + info.response);
+			}
+		  });
 	} catch (e) {
 		console.info(e);
 		res.status(400).json(e);
