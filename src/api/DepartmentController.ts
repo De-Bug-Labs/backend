2c55970dd73bbf8b6acfa609fc3db82f47032679
eduardo <A01704641@itesm.mx>
2021-05-14 13:13:52 -0500

    added find staff by deparment

--- src/api/DepartmentController.ts
+++ src/api/DepartmentController.ts
@@ -1,8 +1,8 @@
 import { getManager } from 'typeorm';
-import { Department } from '../orm/entities';
+import { Department, Staff } from '../orm/entities';
 
 const departmentRepo = getManager().getRepository(Department);
-
+const staffRepo = getManager().getRepository(Staff);
 //Example of CRUD (Create, Read, Update, Delete)
 //implementation of an example entity in TypeORM with swagger
 
@@ -10,6 +10,22 @@ export const consultDepartment = async (req, res): Promise<void> => {
 	try {
 		const departments = await departmentRepo.find();
 		if (departments.length) res.status(200).json(departments);
+		else res.status(404).json({ message: 'index out of bound' });
+	} catch (e) {
+		res.status(400).json(e);
+	}
+};
+
+
+export const cunsultStaffByDepartment = async (req, res): Promise<void> => {
+	try {
+		const staffs = await departmentRepo.find(
+			{ 
+				relations: ["staff"] ,
+				where: {id: req.swagger.params.id.raw }
+			}
+		);
+		if (staffs.length) res.status(200).json(staffs);
 		else res.status(404).json({ message: 'index out of bound' });
 	} catch (e) {
 		res.status(400).json(e);
