d083ce00d678c2e9a3caa59987e32809424197ed
AlbertoMatuteB <a01704584@itesm.mx>
2021-05-15 16:14:40 -0500

    fix test otravez

--- src/api/CalendarController.ts
+++ src/api/CalendarController.ts
@@ -28,30 +28,24 @@ export const consultCalendar = async (req, res): Promise<void> => {
 		const week = req.swagger.params.week.raw || -1;
 		const month = req.swagger.params.month.raw || '';
 		const year = req.swagger.params.year.raw || '';
-		if(week != -1){	
-			const initW = ('0' + ((7 * (week-1)) + 1)).slice(-2);
-			const endW = ('0' + (7 * (week) + 1)).slice(-2);
+		if (week != -1) {
+			const initW = ('0' + (7 * (week - 1) + 1)).slice(-2);
+			const endW = ('0' + (7 * week + 1)).slice(-2);
 			const currentDate = new Date();
 			const currentMonth = ('0' + (currentDate.getUTCMonth() + 1)).slice(-2); //months from 1-12
 			const currentYear = currentDate.getUTCFullYear();
-			console.log(week);
-			console.log(currentMonth);
-			console.log(currentYear);
-			console.log(initW);
-			console.log(endW);
 			const events = await calendarRepo.find({
-			where: `date::text BETWEEN '${currentYear}-${currentMonth}-${initW}' AND '${currentYear}-${currentMonth}-${endW}' ORDER BY date DESC`,
-			take: pageSize, 
-			skip: (page - 1) * pageSize 
-		});
-		if (events.length) res.status(200).json(events);
-		else res.status(404).json({ message: 'index out of bound' });
-	}
-		else{
+				where: `date::text BETWEEN '${currentYear}-${currentMonth}-${initW}' AND '${currentYear}-${currentMonth}-${endW}' ORDER BY date DESC`,
+				take: pageSize,
+				skip: (page - 1) * pageSize,
+			});
+			if (events.length) res.status(200).json(events);
+			else res.status(404).json({ message: 'index out of bound' });
+		} else {
 			const events = await calendarRepo.find({
 				where: `date::text LIKE '${year}%-%${month}-%${day}%' ORDER BY date DESC`,
-				take: pageSize, 
-				skip: (page - 1) * pageSize 
+				take: pageSize,
+				skip: (page - 1) * pageSize,
 			});
 			if (events.length) res.status(200).json(events);
 			else res.status(404).json({ message: 'index out of bound' });
--- tests/orm/calendar.test.ts
+++ tests/orm/calendar.test.ts
@@ -16,7 +16,7 @@ test('create calendar event', async () => {
 	event.description =
 		'a República Mexicana está plagada de grandes platillos, sabores y texturas que varían de región a región y de ciudad a ciudad. Si bien hay muchos restaurantes y oferta gastronómica centralizada en la Ciudad de México, también hay ciudades como Guadalajara, Monterrey, Puebla y León que tienen ofertas culinarias de lo más interesantes, tanto a nivel educativo como a nivel tradición. La ciudad de Querétaro ha visto un crecimiento impresionante en los últimos años, razón por la cual su oferta gastronómica ha ido maximizándose también. Hay varios lugares que cuentan con cursos para aficionados de la cocina, así como universidades que tienen diversos grados de especialidad si es que eliges hacer de tu pasión por la gastronomía un estilo de vida.';
 	event.address = 'centro de queretaro';
-	event.date = new Date().toString();
+	event.date = '2021-05-30';
 	event.srcimg = 'img/hola.jpg';
 	const res = await getRepository(Calendar).save(event);
 	const checkEvent = await getRepository(Calendar).findOne({
