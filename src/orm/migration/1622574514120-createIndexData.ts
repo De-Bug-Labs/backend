b8e49f64e565ce43ea007d13e43b95e023feda9e
Bernardo Estrada <estradaf.bernardo@gmail.com>
2021-11-07 21:31:19 -0600

    Fix migration order

--- src/orm/migration/1622574514120-createIndexData.ts
+++ src/orm/migration/1622574514120-createIndexData.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class createIndexData1636318251369 implements MigrationInterface {
-	name = 'createIndexData1636318251369';
+export class createIndexData1622574514120 implements MigrationInterface {
+	name = 'createIndexData1622574514120';
 
 	public async up(queryRunner: QueryRunner): Promise<void> {
 		await queryRunner.query('ALTER TABLE "information" ADD "indexText" text default \'GAAP\' NOT NULL');
